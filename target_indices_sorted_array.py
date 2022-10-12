c4ea91cd4cba2dd514b8448de58eb93bc66fba25
firi193 <56671623+firi193@users.noreply.github.com>
2022-10-12 21:29:33 +0300

    Create target_indices_sorted_array.py

---
+++ target_indices_sorted_array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        output=[]
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i]==target:
+                output.append(i)
+        output.sort()
+        return output
